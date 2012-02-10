9495290809aa3dfa90adf231fe6ee1084b39ab8c
Lee Wei <evandrix@gmail.com>
2012-02-10 04:06:14 +0000

    updates

---
+++ scala/effectivescala/publish.sh
@@ -0,0 +1,30 @@
+#!/bin/bash 
+
+if [ $# -eq 0 ]; then
+  echo 'you must have something to publish' 1>&2
+  exit 1
+fi
+
+for f; do
+  if [ ! -f $f ]; then
+    echo $f' is not a valid file!' 1>&2
+    exit 1
+  fi
+done
+
+root=$(
+  cd $(dirname $0)
+  /bin/pwd
+)
+
+dir=$1
+out=/tmp/effectivescala.$$
+
+trap "rm -fr $out" 0 1 2
+
+git clone -b gh-pages git@github.com:twitter/effectivescala.git $out
+cp $* $out
+cd $out
+git add .
+git commit -am"publish by $USER"
+git push origin gh-pages
